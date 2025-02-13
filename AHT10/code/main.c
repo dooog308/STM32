4b3d23c88b74328d329ec3eb4025d3f16a1d27f8
dooog308 <pengc5870@gmail.com>
2025-02-13 17:09:51 +0800

    Update main.c

--- AHT10/code/main.c
+++ AHT10/code/main.c
@@ -4,7 +4,7 @@
 #include <AHT10.h>//我寫的AHT10感測器驅動程式
 
 // 配置系統時鐘
-void SystemClock_Config(void);
+void clock_init(void);
 // 初始化GPIO
 void gpio_init(void);
 // 初始化I2C
@@ -64,8 +64,8 @@ void PRINTFI(uint32_t tx)
 }
 int main(void)
 {
-  HAL_Init();
-  SystemClock_Config();
+  HAL_Init();//為了使用HAL_Delay()
+  clock_init();
   gpio_init();
   usart_init();
   i2c_init();
@@ -78,53 +78,27 @@ int main(void)
 
   }
 }
-
-/**
-  * @brief System Clock Configuration
-  * @retval None
-  */
-void SystemClock_Config(void)
+/*使用HSE
+ * SYSCLK= 72MHZ
+ * HCLK=   72MHz
+ * PCLK1=  36MHz
+ * PCLK2=  72MHz
+ */
+void clock_init(void)
 {
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
-
-  /** Configure the main internal regulator output voltage
-  */
-  __HAL_RCC_PWR_CLK_ENABLE();
-  __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE3);
-
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
-  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
-  RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
-  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSI;
-  RCC_OscInitStruct.PLL.PLLM = 8;
-  RCC_OscInitStruct.PLL.PLLN = 50;
-  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
-  RCC_OscInitStruct.PLL.PLLQ = 3;
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_1) != HAL_OK)
-  {
-    Error_Handler();
-  }
+	RCC -> CR |= (1<<16);//HSE ENABLE
+	while(!(RCC -> CR & 0x20000));//wait
+	FLASH -> ACR |= (1<<8);//prefetch enable
+	FLASH -> ACR |= (0x1);
+	RCC -> CFGR |= (0x4<<10);
+	RCC -> PLLCFGR &= ~(0x7fff);
+	RCC -> PLLCFGR |= (1<<22);
+	RCC -> PLLCFGR |= (0x48<<6);
+	RCC -> PLLCFGR |= (0x4);
+	RCC -> CR |= (1<<24);//PLL enable
+	while(!(RCC -> CR & 0x2000000));//wait
+	RCC -> CFGR |= (0x2);
 }
-
 void gpio_init(void)
 {
 	RCC -> AHB1ENR |= (1);//GPIOA CLOCK
